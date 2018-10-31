51e7381ec80616256b9d3e286683640fc7fa9efd
Lance Gundersen <LanceGundersen@users.noreply.github.com>
2018-10-30 22:59:08 -0400

    Create GUI and implement core GUI

--- src/edu/seaport/SeaPortProgram.java
+++ src/edu/seaport/SeaPortProgram.java
@@ -1,10 +1,54 @@
 package edu.seaport;
 
 import javax.swing.*;
+import java.awt.*;
+import java.io.File;
 
 public class SeaPortProgram extends JFrame {
 
-    // TODO: Add GuI
+    private World world;
 
-    World world;
+    private SeaPortProgram() {
+
+        File file = this.getLocalFile();
+        if(file != null) {
+            this.createGui();
+        } else {
+            System.exit(0);
+        }
+    }
+
+    private File getLocalFile() {
+        JFileChooser fileChooser = new JFileChooser(".");
+        fileChooser.showOpenDialog(null);
+        fileChooser.setVisible(true);
+        return fileChooser.getSelectedFile();
+    }
+
+
+    private void createGui() {
+
+        JFrame frame = new JFrame("SeaPort Program");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel();
+
+        BoxLayout boxlayout = new BoxLayout(panel, BoxLayout.Y_AXIS);
+        panel.setLayout(boxlayout);
+
+        JTextArea exampleText = new JTextArea("Hello World!");
+        exampleText.setFont (new java.awt.Font ("Monospaced", Font.PLAIN, 12));
+
+        panel.add(exampleText);
+
+        frame.add(panel);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        new SeaPortProgram();
+    }
 }
